e538e831a6ef94e2c195ee84c0a707f186347c65
J. Ryan Conklin <jryanconklin@gmail.com>
2016-09-08 20:50:44 -0700

    Add email and country properties and create getter and setter methods for contact class.

--- src/contact.php
+++ src/contact.php
@@ -4,27 +4,107 @@
     // Properties
         private $name;
         private $phone_number;
+        private $email;
         private $street;
         private $city;
         private $state;
         private $zip;
+        private $country;
 
     // Constructor
 
-        function _construct($name, $phone_number, $street, $city, $state, $zip)
+        function _construct($name, $phone, $email, $street, $city, $state, $zip, $country)
         {
             $this->name = $name;
-            $this->phone_number = $phone_number;
+            $this->phone = $phone;
+            $this->email = $email;
             $this->street = $street;
-            $this->city = $state;
+            $this->city = $city;
             $this->state = $state;
             $this->zip = $zip;
+            $this->country = $country;
         }
 
     // Getter and Setter Methods
+        //Name
+        function getName()
+        {
+            return $this->name;
+        }
 
+        function setName($new_name)
+        {
+            $this->name = (string) $new_name;
+        }
+        //Phone
+        function getPhone()
+        {
+            return $this->phone;
+        }
 
+        function setPhone($new_phone)
+        {
+            $this->phone = (integer) $new_phone;
+        }
+        //Email
+        function getEmail()
+        {
+            return $this->email;
+        }
 
+        function setEmail($new_email)
+        {
+            $this->email = (string) $new_email;
+        }
+        //Street
+        function getStreet()
+        {
+            return $this->street;
+        }
+
+        function setStreet($new_street)
+        {
+            $this->street = (string) $new_street;
+        }
+        //City
+        function getCity()
+        {
+            return $this->city;
+        }
+        function setCity($new_city)
+        {
+            $this->city = (string) $new_city;
+        }
+        //State
+        function getState()
+        {
+            return $this->state;
+        }
+        function setState($new_state)
+        {
+            $this->state = (string) strtoupper($new_state);
+        }
+        //Zip
+        //Phone
+        function getZip()
+        {
+            return $this->zip;
+        }
+
+        function setZip($new_zip)
+        {
+            $this->zip = (integer) $new_zip;
+        }
+        //Country
+        function getCountry()
+        {
+            return $this->country;
+        }
+
+        function setCountry($new_country)
+        {
+            $this->country = $new_country;
+        }
 
     // General Methods
 
