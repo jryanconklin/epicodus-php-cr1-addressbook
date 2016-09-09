6e685c5022cd4b6241f29e0780dd2ba1e20ae10e
J. Ryan Conklin <jryanconklin@gmail.com>
2016-09-08 18:59:40 -0700

    Add Contact.php formatting and add private properties.

---
+++ src/contact.php
@@ -0,0 +1,30 @@
+<?php
+    class Contact
+    {
+    // Properties
+        private $name;
+        private $phone_number;
+        private $street;
+        private $city;
+        private $state;
+        private $zip;
+
+    // Constructor
+
+
+
+    // Getter and Setter Methods
+
+
+
+
+    // General Methods
+
+
+
+
+    // Static Methods
+
+
+    }
+?>
